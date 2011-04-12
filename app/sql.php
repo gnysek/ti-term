5b83491f0423aa84cd020ddcd3c75f14a1526ba7
gnysek <gnysek@gnysek-vaio.dyn.inf.ug.edu.pl>
2011-04-12 12:42:33 +0200

    - sql buildier

--- app/sql.php
+++ app/sql.php
@@ -7,9 +7,8 @@ class Sql {
 	const TYPE_INSERT = 4;
 	const TYPE_REPLACE = 5;
 
-	private $_type = 0;
-	private $_select = '';
-	private $_from = '';
+	private $_type = 1;
+	private $_columns = '';
 	private $_where = '';
 	private $_from = '';
 	private $_join = '';
@@ -17,16 +16,23 @@ class Sql {
 	private $_group = '';
 	private $_order = '';
 	private $_limit = '';
+	// bufory
 	private $_sqlPrepared = FALSE;
 	private $_finalSql = '';
+	private $_count = NULL;
+	private $_collection = NULL;
 
+	/**
+	 * Przygotowuje string z SQLem
+	 * @return Sql 
+	 */
 	private function _prepareSql() {
 		if ($this->_sqlPrepared)
 			return $this;
 
 		switch ($this->_type) {
 			case SELECT:
-				$this->_finalSql .= 'SELECT ' . ((empty($this->_select)) ? '*' : $this->_select)
+				$this->_finalSql .= 'SELECT ' . ((empty($this->_columns)) ? '*' : $this->_columns)
 					. ' FROM ' . $this->_from;
 				if (!empty($this->_where))
 					$this->_finalSql .= ' WHERE ' . $this->_where;
@@ -42,24 +48,36 @@ class Sql {
 		return $this;
 	}
 
-	public function select() {
-		
+	public function columns($columns) {
+		if (is_array($columns)) {
+			$this->_columns = implode(', ', $columns);
+		} else {
+			$this->_columns = $columns;
+		}
 	}
 
 	public function from($from) {
-		$this->_from = $from;
+		if (is_array($select)) {
+			$this->_from = implode(', ', $from);
+		} else {
+			$this->_from = $from;
+		}
 		return $this;
 	}
 
 	public function load() {
-		// jeśli nie wiadomo skąd pobieramy, to jak w ogóle cokolwiek pobrać ?
-		if (empty($this->_from)) {
-			trigger_error('Nie podano tabeli, nie mozna wykonac zapytania.');
+		if ($this->_collection === NULL) {
+			// jeśli nie wiadomo skąd pobieramy, to jak w ogóle cokolwiek pobrać ;)
+			if (empty($this->_from)) {
+				trigger_error('Nie podano tabeli, nie mozna wykonac zapytania.');
+			}
+
+			$this->_prepareSql();
+
+			$this->_collection = DB::query($this->_finalSql);
 		}
-
-		$this->_prepareSql();
-
-		return DB::query($this->_finalSql);
+		
+		return $this->_collection;
 	}
 
 	/**
@@ -75,8 +93,14 @@ class Sql {
 	 * @return Sql 
 	 */
 	public function count() {
-		// zamienia SELECT * na SELECT COUNT(*)
-		return $this;
+		if ($this->_count === NULL) {
+			// zamienia SELECT * na SELECT COUNT(*)
+			$count_sql = $sql = preg_replace('/select\s+.+?\s+from\s+/is', 'select count(*) as count from ', $this->_sqlPrepared);
+			$result = DB::query($count_sql);
+			$this->_count = $result->count;
+		}
+
+		return $this->_count;
 	}
 
 }
