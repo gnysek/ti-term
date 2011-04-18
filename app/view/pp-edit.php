2cec6211db401276ef5ca93893f58e01d4cca360
gnysek <gnysek@gnysek-vaio>
2011-04-19 00:26:45 +0200

    - dodawanie i usuwanie normalne + ajaxowe

--- app/view/pp-edit.php
+++ app/view/pp-edit.php
@@ -2,18 +2,33 @@
 
 <script type="text/javascript">
 	var ppId = '<?php echo $data->id; ?>';
+	var token = '';
+	
+	$().ready(function(){
+		$('#slide-add a').click(function(){
+			$('#slide-list').load( $(this).attr('href') );
+			$().focus();
+			return false;
+		});
+		
+		$('#slide-list .slide-del').live('click', function(){
+			$('#slide-list').load( $(this).attr('href') );
+			$().blur();
+			return false;
+		});
+	});
 </script>
 
 <div class="left-column">
 	Lista slajdów
-	<div></div>
-
+	<hr/>
 	<div id="slide-list">
-		<?php echo $this->render('pp-left-slide'); ?>
+		<?php echo $this->render('pp-left-slide', array('slajdy' => $slajdy, 'id' => $data->id)); ?>
 	</div>
-
+	<hr/>
 	<div id="slide-add">
 		<a href="<?php echo Core::request()->getUrl('pp/slide', array('id' => $data->id)); ?>"><img src="<?php echo Core::request()->getHost(); ?>media/img/layer_add.png"/></a>
+		Dodaj slajd
 	</div>
 
 </div>
@@ -22,4 +37,4 @@
 </div>
 <div class="clearfix"></div>
 
-<input id="token"/>
+<input id="token" type="hidden"/>
--- app/view/pp-left-slide.php
+++ app/view/pp-left-slide.php
@@ -1,1 +1,8 @@
-cokolwiek dla testu
+<?php $i = 0;
+foreach ($slajdy as $v): ?>
+	<img src="<?php echo Core::request()->getHost(); ?>media/img/layer_edit.png"/>
+	<?php echo++$i; ?>.
+	<a class="slide-del" href="<?php echo Core::request()->getUrl('pp/delslide', array('id' => $id, 'slid' => $v->id)); ?>"><img src="<?php echo Core::request()->getHost(); ?>media/img/delete.png"/></a>
+	<br/>
+
+<?php endforeach; ?>
