d5df9d0da67ad715f3534e9dc2e5a403d7fe6776
gnysek <gnysek@gnysek-vaio>
2011-05-16 22:04:51 +0200

    - zasrany bbcode

--- app/BBcode.php
+++ app/BBcode.php
@@ -11,6 +11,8 @@ class BBCode {
 		$text = preg_replace('#\[img\](.*?)\[/img]#si','<img src="$1" alt="Obrazek"/>', $text);
 		$text = preg_replace('#\[\*\](.*?)\[/\*\]#si','<li>$1</li>', $text);
 		$text = preg_replace('#\[font size="(.*?)"\](.*?)\[/font\]#si','<span style="font-size: $1em">$2</span>', $text);
+		$text = preg_replace('#\[size=(.*?)\](.*?)\[/size\]#si','<font size="$1">$2</font>', $text);
+		$text = preg_replace('#\[color=(.*?)\](.*?)\[/color\]#si','<span style="color: $1">$2</span>', $text);
 		$text = preg_replace('#\[h1\](.*?)\[/h1\]#si','<h1>$1</h1>', $text);
 		$text = nl2br($text);
 		return $text;
